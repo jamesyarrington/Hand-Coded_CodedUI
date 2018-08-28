afbe26f10276a3d7b2cd745a326d5c84af4b40d6
James Yarrington <jyarrington@vt.edu>
2018-08-28 09:24:07 -0400

    Created base CallFun class

---
+++ Hand_Coding_Tutorial/CalciFun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hand_Coding_Tutorial
+{
+
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [CodedUITest]
+    class CalciFun
+    {
+        static UITestControl control = Launch.Control();
+
+        [TestMethod]
+        public void CallFun()
+        {
+
+        }
+    }
+}
