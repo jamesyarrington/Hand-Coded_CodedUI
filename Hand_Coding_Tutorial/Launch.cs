a3da4daa3dc874a57461dbd04ba54d75c1f1dad1
James Yarrington <jyarrington@vt.edu>
2018-08-28 09:17:09 -0400

    created Launch Class

---
+++ Hand_Coding_Tutorial/Launch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hand_Coding_Tutorial
+{
+
+    using Microsoft.VisualStudio.TestTools.UITesting;
+
+    class Launch
+    {
+        public static UITestControl calciwindow;
+
+        public static UITestControl Control()
+        {
+            calciwindow = new UITestControl();
+            calciwindow.TechnologyName = "MSAA";
+            calciwindow.SearchProperties[UITestControl.PropertyNames.Name] = "Calculator";
+            calciwindow.SearchProperties[UITestControl.PropertyNames.ClassName] = "CalcFrame";
+            return calciwindow;
+        }
+
+    }
+}
